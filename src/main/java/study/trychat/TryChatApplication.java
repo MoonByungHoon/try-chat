ce93194980edf577e1d59bfde9fb95e304dceb95
MoonByungHoon <mbh1069@naver.com>
2024-04-26 09:57:24 +0900

    push test

---
+++ src/main/java/study/trychat/TryChatApplication.java
@@ -0,0 +1,13 @@
+package study.trychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/trychat/TryChatApplicationTests.java
@@ -0,0 +1,13 @@
+package study.trychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
