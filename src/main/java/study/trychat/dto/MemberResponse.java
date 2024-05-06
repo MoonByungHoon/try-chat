a3e2b46caf4254989d3f4def604fd5b4cafba918
MoonByungHoon <mbh1069@naver.com>
2024-05-07 06:54:00 +0900

    새로 만든 MemberResponse 기본 생성자 누락 적용.

--- src/main/java/study/trychat/dto/MemberResponse.java
+++ src/main/java/study/trychat/dto/MemberResponse.java
@@ -4,12 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Builder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class MemberResponse {
 
