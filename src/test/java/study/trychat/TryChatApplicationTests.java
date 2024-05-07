2a64b19c4875a01855985b41f2de02190e8903ec
MoonByungHoon <mbh1069@naver.com>
2024-05-07 22:14:42 +0900

    사용하지 않는 테스트 코드 삭제.

--- src/test/java/study/trychat/TryChatApplicationTests.java
+++ src/test/java/study/trychat/TryChatApplicationTests.java
@@ -1,13 +1,8 @@
 package study.trychat;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TryChatApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
 
 }
